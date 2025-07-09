fb43b61b612a2faa245dc63be52d7a9654e85140
anonymousecode <anonymousecode1@gmail.com>
2025-07-10 00:28:01 +0530

    added recipe list, added dockerfile for render

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,19 +8,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
+  <div class="container">
     <div class="header row">
-        <div class="col">
-            <img src="{{asset('images/github.png')}}" class="img-fluid float-end" style="width:40px;" alt="">
-        </div>
-
+      <div class="col d-flex justify-content-end align-items-center">
+        <img src="{{ asset('images/github.png') }}" class="img-fluid" style="width:40px;" alt="">
+      </div>
     </div>
+
     <div class="row justify-content-center">
-        <div class="col-auto">
-            {{ $content }}
-        </div>
+      <div class="col-auto">
+        {{ $content }}
+      </div>
     </div>
-    <div class="footer row">
 
+    <div class="footer row">
+      <p>Copyright</p>
     </div>
+  </div>
 </body>
+
 </html>
--- resources/views/recipelist.blade.php
+++ resources/views/recipelist.blade.php
@@ -1,3 +1,37 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>CookaVel</title>
+</head>
+<body>
+  <x-layout>
+    <x-slot name="content">
+      <div class="container py-4">
+        <h2 class="mb-4">Results for {{ $input }}</h2>
+
+        <div class="row g-4">
+          @for ($i = 0; $i < count($data['meals']); $i++)
+            <div class="col-md-4">
+              <div class="card h-100 shadow-sm">
+                <a href="{{ route('fromRecipeList', $data['meals'][$i]['idMeal']) }}" class="text-decoration-none text-dark">
+                  <img
+                    src="{{ $data['meals'][$i]['strMealThumb'] }}"
+                    class="card-img-top"
+                    alt="Image of {{ $data['meals'][$i]['strMeal'] }}"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">{{ $data['meals'][$i]['strMeal'] }}</h5>
+                  </div>
+                </a>
+              </div>
+            </div>
+          @endfor
+        </div>
+      </div>
+    </x-slot>
+  </x-layout>
+
+</body>
+</html>
